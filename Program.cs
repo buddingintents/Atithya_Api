5dc3b554a115cc818ee1fd9aaa6f07ba163dc089
Ankit Sharma <buddingintents@google.com>
2024-02-25 21:14:12 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,84 @@
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
+namespace Atithya_Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            // Using the GetValue<type>(string key) method
+
+            var configValue = builder.Configuration.GetValue<string>("JWT:Key");
+
+            builder.Services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(o =>
+            {
+                var Key = Encoding.UTF8.GetBytes(builder.Configuration.GetValue<string>("JWT:Key"));// Configuration["JWT:Key"]);
+                o.SaveToken = true;
+                o.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration.GetValue<string>("JWT:Issuer"),// Configuration["JWT:Issuer"],
+                    ValidAudience = builder.Configuration.GetValue<string>("JWT:Audience"),//Configuration["JWT:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Key)
+                };
+            });
+
+            builder.Services.AddSingleton<IJWTManagerRepository, JWTManagerRepository>();
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition("Token", new OpenApiSecurityScheme
+                {
+                    Description = "Enter a valid token in the text input below.",
+                    Name = "x-token",
+                    In = ParameterLocation.Header,
+                });
+            });
+
+            // Add services to the container.
+            var allowedOrigin = builder.Configuration.GetSection("AllowedOrigins").Get<string[]>();
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("myAppCors", policy =>
+                {
+                    policy.WithOrigins(allowedOrigin)
+                            .AllowAnyHeader()
+                            .AllowAnyMethod();
+                });
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.UseCors("myAppCors");
+            app.Run();
+        }
+    }
+}
